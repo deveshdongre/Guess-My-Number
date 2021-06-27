f4b2712c4f0f1bf5c4f6e5a332b088babe963519
deveshdongre <devesh.dongre@gmail.com>
2021-06-27 23:27:59 +0530

    Manuipulating style using JS

--- script.js
+++ script.js
@@ -6,21 +6,36 @@ console.log(number);
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value)
     console.log(guess);
-
+    // case when there is now input
     if (!guess) {
         document.querySelector('.message').textContent = 'No Number';
-    } else if (guess === number) {
+    } else if (guess === number) {                      //when Number guessed correctly
         document.querySelector('.message').textContent = 'Congratulation you guessed it correctly';
-    } else if (guess > number) {
-        score -= 1;
-        console.log(`now score ${score}`);
-        document.querySelector('.message').textContent = 'Too High';
-        document.querySelector('.score').textContent = score;
-    } else if (guess < number) {
-        score -= 1;
-        console.log(`now score ${score}`);
-        document.querySelector('.message').textContent = 'Too Low';
-        document.querySelector('.score').textContent = score;
+        document.querySelector('.number').textContent = number;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+    } else if (guess > number) {                        //Number is greater than the guess
+        if (score > 1) {
+            score -= 1;
+
+            document.querySelector('.message').textContent = 'Too High';
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = 'You lost the Game';
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < number) {                        //Number is smaller than the guess
+        if (score > 1) {
+            score -= 1;
+            document.querySelector('.message').textContent = 'Too Low';
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = 'You lost the Game';
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
 
