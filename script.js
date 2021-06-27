dabb721ff0ebd8efc71e0d36252231611d9edf95
deveshdongre <devesh.dongre@gmail.com>
2021-06-27 23:01:29 +0530

    gessing DOM manipulation added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 const number = Math.trunc(Math.random() * 20) + 1;
+let score = Number(20);
+
 console.log(number);
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value)
@@ -9,6 +11,16 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.message').textContent = 'No Number';
     } else if (guess === number) {
         document.querySelector('.message').textContent = 'Congratulation you guessed it correctly';
+    } else if (guess > number) {
+        score -= 1;
+        console.log(`now score ${score}`);
+        document.querySelector('.message').textContent = 'Too High';
+        document.querySelector('.score').textContent = score;
+    } else if (guess < number) {
+        score -= 1;
+        console.log(`now score ${score}`);
+        document.querySelector('.message').textContent = 'Too Low';
+        document.querySelector('.score').textContent = score;
     }
 });
 
